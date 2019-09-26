715a966d3f2eaeb349d00516504742c97fdb19ed
Cristian Rodriguez <cristian.rr@coditramuntana.com>
2019-09-26 12:08:13 +0200

    [CONF] Set TimeZone configuration.

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module DiputacioGironaApp
 
     # Cannot force time_zone as all timestamps in DB are in UTC
     # and ought to be updated if this config was applied.
-    # config.time_zone = 'Madrid'
+    config.time_zone = 'Madrid'
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
