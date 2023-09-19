bdb057331312bd98427c0656cff3a68769b377b6
diputaciodegirona <desprog@ddgi.cat>
2023-09-19 09:52:23 +0200

    Canviat la manera d'escriure les claus

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe I18n do
 
   it 'does not have unused keys' do
     expect(unused_keys).to be_empty,
-                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them.....\n#{unused_keys.leaves.map { |path| "  #{path}" }.join("\n")}\n"
+                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them.....\n#{unused_keys.leaves.map { |path| "  #{path.key}" }.join("\n")}\n"
   end
 
   it 'files are normalized' do
