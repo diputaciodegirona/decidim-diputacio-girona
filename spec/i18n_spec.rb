2431f7e1176c031fab540d97b7836efc4ff156f2
diputaciodegirona <desprog@ddgi.cat>
2023-09-19 09:35:50 +0200

    Prova per a mirar de detectar què troba

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe I18n do
 
   it 'does not have unused keys' do
     expect(unused_keys).to be_empty,
-                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them..... #{unused_keys.leaves.join(', ')}"
   end
 
   it 'files are normalized' do
