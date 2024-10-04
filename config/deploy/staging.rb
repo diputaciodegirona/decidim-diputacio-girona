99eb1fd13ba185004001924904ee6f5f1ee022bb
Oscar Pérez del Campo <operez@ddgi.cat>
2024-10-04 08:58:03 +0200

    Configurat la branca a desplegar

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, 'staging'
-set :branch, 'master'
+set :branch, 'decicim-0.28'
 
 set :git_http_password, " 2GThJ7dJgSNnyQBr"
 
@@ -7,7 +7,7 @@ server '195.93.170.50', user: 'decidim', port: '22', roles:  %w{app web db}
 set :deploy_to, "/var/www/decidim/ddgi/" # directory for the project in the server, in this case 'var/www/customers'
 
 set :rbenv_type, :user
-set :rbenv_ruby, '3.0.2'
+set :rbenv_ruby, '3.1.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all # default value
